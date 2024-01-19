a68f75d0c280686765b2eb1ae09984b2f3eddf3a
Sonja Lerchl <sosobee@web.de>
2024-01-19 11:07:45 +0100

    colors change random

--- js-structure_shapes/index.js
+++ js-structure_shapes/index.js
@@ -7,22 +7,19 @@ const root = document.getElementById("root");
 const circle = document.createElement("div");
 circle.classList.add("circle");
 circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
-  getRandomColor();
+  circle.style.backgroundColor = getRandomColor();
 });
 
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
-  getRandomColor();
+  square.style.backgroundColor = getRandomColor();
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
-  getRandomColor();
+  pentagon.style.backgroundColor = getRandomColor();
 });
 
 root.append(circle, square, pentagon);
