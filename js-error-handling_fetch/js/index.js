8aed2b8d7631e096a126b6fb9dfc613fd71c6925
Sonja Lerchl <sosobee@web.de>
2024-01-24 12:00:10 +0100

    solve challenge

--- js-error-handling_fetch/js/index.js
+++ js-error-handling_fetch/js/index.js
@@ -1,16 +1,34 @@
 console.clear();
 
+const errorElement = document.querySelector('[data-js="errorElement"]');
+
 const userElement = document.querySelector(".user");
 
 async function getUser(url) {
   const response = await fetch(url);
-  const json = await response.json();
-  return json.data;
+  if (!response.ok) {
+    console.log("Network error");
+    return null;
+  }
+  try {
+    const json = await response.json();
+    return json.data;
+  } catch (error) {
+    console.log("error");
+    return null;
+  }
 }
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
     const user = await getUser(event.target.dataset.url);
+    errorElement.innerHTML = "";
+    userElement.innerHTML = "";
+    if (!user) {
+      console.log("User not found");
+      errorElement.textContent = "User not found.";
+      return;
+    }
     userElement.innerHTML = `
     <h2>${user.first_name} ${user.last_name}</h2>
     <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
