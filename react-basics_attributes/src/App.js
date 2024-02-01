3972a6b2211696ed3ee8c0a032b2194a7d5eccf3
Sonja Lerchl <sosobee@web.de>
2024-02-01 12:09:28 +0100

    solve challenge

--- react-basics_attributes/src/App.js
+++ react-basics_attributes/src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <ThisIsAnArticle />;
+}
+
+function ThisIsAnArticle() {
+  return (
+    <article className="article">
+      <h2 className="article__title">This is an articles title</h2>
+      <label htmlFor="input">type in whatever you like</label>
+      <input id="input" />
+      <a className="article__link" href="https://de.wikipedia.org/wiki/Dog">
+        Dogs
+      </a>
+    </article>
+  );
 }
