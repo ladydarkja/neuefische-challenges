7607f3871e4402af0bbe8bf6b5ce862c22cb4873
Sonja Lerchl <sosobee@web.de>
2024-02-02 11:51:19 +0100

    solve challenge

--- react-props_greeting/src/App.js
+++ react-props_greeting/src/App.js
@@ -1,5 +1,14 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Sonja" isCoach={true} />;
+}
+
+function Greeting({ name, isCoach }) {
+  return (
+    <div>
+      Hello, {isCoach ? <span>Coach </span> : null}
+      {name}!
+    </div>
+  );
 }
