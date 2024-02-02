dfd51c4416d2ad73985e54b2fd5cb12faf4aae83
Sonja Lerchl <sosobee@web.de>
2024-02-02 12:34:40 +0100

    solved challenge

--- react-props_smiley/src/App.js
+++ react-props_smiley/src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Smiley isHappy={true} />
+      <Smiley isHappy={false} />
+    </div>
+  );
+}
+
+function Smiley({ isHappy }) {
+  return (
+    <div>
+      {isHappy ? (
+        <span aria-label="happy emoji">😊</span>
+      ) : (
+        <span aria-label="sad emoji">😔</span>
+      )}
+    </div>
+  );
 }
