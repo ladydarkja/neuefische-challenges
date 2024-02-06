a4f1419b350fa7c256ab8235fe415ad00a4da798
Sonja Lerchl <sosobee@web.de>
2024-02-06 11:40:55 +0100

    solved challenge

--- react-state_counter/src/App.js
+++ react-state_counter/src/App.js
@@ -1,27 +1,25 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
 
+  function handleClickPlus() {
+    setCount(count + 1);
+  }
+
+  function handleClickMinus() {
+    setCount(count - 1);
+  }
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleClickMinus}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleClickPlus}>
           +
         </button>
       </div>
