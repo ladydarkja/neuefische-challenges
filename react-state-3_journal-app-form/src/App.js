5ea9b1346c8d44a67c733a281082e9dcf6756ac4
Sonja Lerchl <sosobee@web.de>
2024-02-12 10:25:25 +0100

    solved challenge

--- react-state-3_journal-app-form/src/App.js
+++ react-state-3_journal-app-form/src/App.js
@@ -3,6 +3,7 @@ import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { uid } from "uid";
 
 function App() {
   return (
