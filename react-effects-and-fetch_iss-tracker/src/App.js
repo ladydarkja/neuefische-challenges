ef2b088818eb5782fde3dc4394c92b76213066b1
Sonja Lerchl <sosobee@web.de>
2024-02-12 11:32:46 +0100

    solved challenge

--- react-effects-and-fetch_iss-tracker/src/App.js
+++ react-effects-and-fetch_iss-tracker/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
@@ -11,7 +11,26 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setCoords({ longitude: data.longitude, latitude: data.latitude });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getISSCoords();
+
+    const intervall = setInterval(() => {
+      getISSCoords();
+    }, 5000);
+    return () => clearInterval(intervall);
+  }, []);
+
+  console.log(coords);
 
   return (
     <main>
